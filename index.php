45dc138f28f3fb28c0dc9034b73bfa527ef3bd19
robialakbar <robialakbar@gmail.com>
2021-05-13 19:05:15 +0700

    Belajar Numbers

--- index.php
+++ index.php
@@ -43,4 +43,23 @@ echo "<br>";
 $string = "robi al akbar";
 $output = " {$string} adalah seorang \"Programmer\"";
 echo $output;
+echo "<br>";
+
+/**
+ * Belajar Numbers -> Kita Bisa mengecek apakah variable tersebut type float, integer atau string dll dengan metode bool(true / False)
+ */
+$num_1 = 10.1;
+$num_2 = 10;
+$string = "robi";
+echo var_dump(is_float($num_1));
+echo "<br>";
+echo var_dump(is_integer($num_1));
+$ciri = var_dump(is_integer($num_1));
+echo "<br>";
+echo "$num_1 is float $ciri";
+echo "<br>";
+echo "$num_2 is integer";
+echo "<br>";
+echo "$string is String";
 ?>
+
