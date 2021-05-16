4404a47d653ce665e17c8dfbc588ba0200f6e32a
robialakbar <robialakbar@gmail.com>
2021-05-16 14:19:09 +0700

    Belajar Conditions

--- index.php
+++ index.php
@@ -12,20 +12,151 @@ echo "<br>";
 /**
  * Belajar Variable Number
  */
-$number_1 = 80;
-$number_2 = 40;
-$equals = $number_1 + $number_2;
-echo $equals;
-//atau
-echo "<br>";
-echo $number_1 + $number_2;
-echo "<br>";
+$number_1 = 10;
+$number_2 = 10;
+$number_3 = 10;
+$number_4 = 10;
+$number_5 = 10;
+$number_6 = 10;
+$number_7 = 10;
+$number_8 = 10;
+$number_9 = 10;
+$number_10 = 10;
+$number_11 = 10;
+$number_12 = 10;
+$number_11 = 10;
+$number_12 = 10;
+$number_13 = 10;
+$number_14 = 10;
+$number_15 = 10;
+$number_16 = 10;
+$number_17 = 10;
+$number_18 = 10;
+$number_19 = 10;
+$number_20 = 10;
+$number_21 = 10;
+$number_22 = 10;
+$number_22 = 10;
+$number_23 = 10;
+$number_24 = 10;
+$number_25 = 10;
+$number_26 = 10;
+$number_27 = 10;
+$number_28 = 10;
+$number_29 = 10;
+$number_30 = 10;
+$number_31 = 10;
+$number_32 = 10;
+$number_33 = 10;
+$number_34 = 10;
+$number_35 = 10;
+$number_36 = 10;
+$number_37 = 10;
+$number_38 = 10;
+$number_39 = 10;
+$number_40 = 10;
+$number_41 = 10;
+$number_42 = 10;
+$number_43 = 10;
+$number_44 = 10;
+$number_45 = 10;
+$number_46 = 10;
+$number_47 = 10;
+$number_48 = 10;
+$number_49 = 10;
+$number_50 = 10;
+$number_51 = 10;
+$number_51 = 10;
+$number_52 = 10;
+$number_53 = 10;
+$number_54 = 10;
+$number_55 = 10;
+$number_56 = 10;
+$number_57 = 10;
+$number_58 = 10;
+$number_59 = 10;
+$number_60 = 10;
+$number_61 = 10;
+$number_61 = 10;
+$number_62 = 10;
+$number_63 = 10;
+$number_64 = 10;
+$number_65 = 10;
+$number_66 = 10;
+$number_67 = 10;
+$number_68 = 10;
+$number_69 = 10;
+$number_70 = 10;
+$number_71 = 10;
+$number_72 = 10;
+$number_73 = 10;
+$number_74 = 10;
+$number_75 = 10;
+$number_76 = 10;
+$number_77 = 10;
+$number_78 = 10;
+$number_79 = 10;
+$number_80 = 10;
+$number_81 = 10;
+$number_82 = 10;
+$number_83 = 10;
+$number_84 = 10;
+$number_85 = 10;
+$number_86 = 10;
+$number_87 = 10;
+$number_88 = 10;
+$number_89 = 10;
+$number_90 = 10;
+$number_91 = 10;
+$number_92 = 10;
+$number_93 = 10;
+$number_94 = 10;
+$number_95 = 10;
+$number_96 = 10;
+$number_97 = 10;
+$number_98 = 10;
+$number_99 = 10;
+$number_100 = 10;
 
+//Deklarasi Logika
+
+$pertambahan_2 = $number_1 + $number_2 + $number_3 + $number_4 + $number_5 + $number_6 + $number_7 + $number_8 + $number_9 + $number_10 + $number_11 + $number_12 + $number_13 + $number_14 + $number_15 + $number_16 + $number_17 + $number_18 + $number_19 + $number_20 + $number_21 + $number_22 + $number_23 + $number_24 + $number_25 +  $number_26 + $number_27 + $number_28 + $number_29 + $number_30 + $number_31 + $number_32 + $number_33 + $number_34 + $number_35 + $number_36 + $number_37 + $number_38 + $number_39 + $number_40 + $number_41 + $number_42 + $number_43 + $number_44 + $number_45 + $number_46 + $number_47 + $number_48 + $number_49 + $number_50 + $number_51 + $number_52 + $number_53 + $number_54 + $number_55 + $number_56 + $number_57 + $number_58 + $number_59 + $number_60 + $number_61 + $number_62 + $number_63 + $number_64 + $number_65 + $number_66 + $number_67 + $number_68 + $number_69 + $number_70 + $number_71 + $number_72 + $number_73 + $number_74 + $number_75 + $number_76 + $number_77 + $number_78 + $number_79 + $number_80 + $number_81 + $number_82 + $number_83 + $number_84 + $number_85 + $number_86 + $number_87 + $number_88 + $number_89 + $number_90 + $number_91 + $number_92 + $number_93 + $number_94 + $number_95 + $number_96 + $number_97 + $number_98 + $number_99 + $number_100;
+$pertambahan_3 = $number_1 + $number_2;
+$pertambahan_4 = $number_2 + $number_3 + $number_4 + $number_5 + $number_6 + $number_7 + $number_8 + $number_9 + $number_10 + $number_11 + $number_12 + $number_13 + $number_14 + $number_15 + $number_16 + $number_17 + $number_18 + $number_19 + $number_20 + $number_21;
+$total = $pertambahan_4 + $pertambahan_2;
+$total_1 = $total + $pertambahan_2;
+// Tampilkan hasil;
+echo "<br>";
+echo $pertambahan_4;
+echo "<br>";
+echo $total;
+echo "<br>";
+echo $total + $pertambahan_2;
+echo "<br>";
+echo $total_1 + $pertambahan_2 + $pertambahan_3 + $pertambahan_4;
+
+// $penjualan = $number_1 + $number_2 + $number_3 + $number_4 + $number_5 + $number_6 + $number_7 + $number_8 + $number_9;
+// $penjumlahan = $number_1 + $number_2 + $number_3 + $number_4 + $number_5;
+// $pengalihan = $penjualan  - $penjumlahan;
+// #$equals = $number_1 + $number_2;
+// // echo $penjualan;
+// // echo "<br>";
+// // echo $penjumlahan;
+// // echo "<br>";
+// // echo $pengalihan;
+// // //atau
+// // echo "<br>";
+// // echo $penjualan + $penjumlahan;
+// // echo "<br>";
+// // echo $penjumlahan - $penjualan;
+// // echo "<br>";
+// // **/
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * Belajar Variable String
  */
+echo "<br>";
 $text_1 = "robi";
 $text_2 = "al akbar";
 $nama_saya = $text_1."". $text_2;
@@ -34,7 +165,15 @@ echo "<br>";
 //atau
 echo $text_1 .''. $text_2;
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+echo "<br>";
+$huruf_1 = "saya";
+$huruf_2 = "mau";
+$huruf_3 = "menjadi";
+$huruf_4 = "Programmer";
+$dream = $huruf_1 ." ". $huruf_2 ." ". $huruf_3 ." ". $huruf_4;
 
+echo $dream;
 ///////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * Belajar Strings
@@ -44,6 +183,29 @@ $string = "robi al akbar";
 $output = " {$string} adalah seorang \"Programmer\"";
 echo $output;
 echo "<br>";
+
+
+/**
+ * Membuat Pengecualian itu untuk array yang ada 
+ */
+
+$nano = "strinssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssg";
+$perkalian = "string" . "pembagian";
+$personifikasi = "angkatan darat";
+$erlingga = "mengalihkan";
+$pengalihan = "angkatan";
+$untuk = "Pengangkatan";
+$polisi = "pengangkatan";
+$giologi = "sosiology";
+$penalty = "pengangkatan";
+$teory = "emergency";
+$pengangkatan = " Kuota Ikan";
+$ikatan = "ikatan alumni";
+$pengangkatan = "semua";
+
+
+echo ($nano);
+
 
 /**
  * Belajar Numbers -> Kita Bisa mengecek apakah variable tersebut type float, integer atau string dll dengan metode bool(true / False)
@@ -150,6 +312,27 @@ $phone_numbers = [
 //echo var_dump($phone_numbers);
 //echo var_dump(array_keys ($phone_numbers));
 echo var_dump(array_values($phone_numbers));
+
+/**
+ * Conditions
+ */
+echo "<br>";
+$nam = "25";
+$user_Input = "hello";
+
+if (is_int($user_Input)){
+    if($nam == $user_Input){
+        echo " $nam sama dengan $user_Input";
+    } else if($nam <= $user_Input){
+        echo " $nam lebih kecil dari $user_Input";
+    } else if($nam >= $user_Input){
+        echo " $nam lebih besar dari $user_Input";
+    }
+    }
+    else{
+        echo "error Mas Bro";
+    }
+
 
 ?>
 
